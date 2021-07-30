e5d5fbcd58eba91da7853e6d307cac96b9b61598
JackRJr <jack_rieswick@hotmail.com>
2021-07-30 17:27:13 -0300

    first commit: project structure created

---
+++ src/main/java/one/digitalinnovation/everis/clientManagerVeterinaryClinic/ClientManagerVeterinaryClinicApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.everis.clientManagerVeterinaryClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagerVeterinaryClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagerVeterinaryClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/everis/clientManagerVeterinaryClinic/ClientManagerVeterinaryClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.everis.clientManagerVeterinaryClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagerVeterinaryClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
